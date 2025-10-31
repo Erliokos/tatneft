0ef0edc56e04a6b4834a5668502f92cc628e8ebd
Erliokos <albert.murzakov@gmail.com>
2025-10-31 17:29:08 +0300

    TAT-0002: add styled components

---
+++ src/globalStyle.ts
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+  color: blue;
+  text-decoration: underline !important;
+  transition: color 0.2s;
+  }
+
+  a:hover {
+  color: darkblue;
+  }
+
+  html {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.fonts.body};
+    color: ${({ theme }) => theme.colors.secondary[900]};
+    background-color: ${({ theme }) => theme.colors.white};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  #root {
+    min-height: 100vh;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-family: inherit;
+  }
+
+  input, textarea, select {
+    font-family: inherit;
+    border: none;
+    outline: none;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
